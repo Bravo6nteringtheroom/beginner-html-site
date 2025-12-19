63de6863f828f047133a189bbd5c6f8c3a722058
Bravo6nteringtheroom <mohamadOwidate2@gmail.com>
2025-12-19 15:44:10 +0200

    New HTML_Tag Page added that explain how the h1 Tag is used / Adding HoemPage and editing the main index.html page and allowing the user to use the previous and next words to change between pages

---
+++ HTML_Tags/h1 Tag/action.js
@@ -0,0 +1,3 @@
+let h1_tag = document.getElementById("Syntax_tag")
+
+h1_tag.textContent = "<h1>Hello World</h1>"
---
+++ Home.js
@@ -0,0 +1,10 @@
+Prev_btn = document.getElementById("Previous")
+Next_btn = document.getElementById("Next")
+
+Prev_btn.addEventListener('click' , () => {
+  window.location.href = './index.html'
+})
+
+Next_btn.addEventListener('click' , () => {
+  window.location.href = './HTML_Tags/h1 Tag/h1.html'
+})
